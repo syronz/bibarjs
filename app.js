359c5fc401cd8e00b99b504c799fb5c99e5572d3
Diako Amir <sabina.diako@gmail.com>
2021-02-04 23:19:38 +0300

    refactor dict and indexedDB

--- app.js
+++ app.js
@@ -1,103 +1,150 @@
-//prefixes of implementation that we want to test
-window.indexedDB = window.indexedDB || window.mozIndexedDB || 
-  window.webkitIndexedDB || window.msIndexedDB;
 
-//prefixes of window.IDB objects
-window.IDBTransaction = window.IDBTransaction || 
-  window.webkitIDBTransaction || window.msIDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || 
-  window.msIDBKeyRange
+initWords = (initLoadWords) => {
+  //prefixes of implementation that we want to test
+  window.indexedDB = window.indexedDB || window.mozIndexedDB || 
+    window.webkitIndexedDB || window.msIndexedDB;
 
-if (!window.indexedDB) {
-  window.alert("Your browser doesn't support a stable version of IndexedDB.")
-}
+  //prefixes of window.IDB objects
+  window.IDBTransaction = window.IDBTransaction || 
+    window.webkitIDBTransaction || window.msIDBTransaction;
+  window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || 
+    window.msIDBKeyRange
+
+  if (!window.indexedDB) {
+    window.alert("Your browser doesn't support a stable version of IndexedDB.")
+  }
 
 
-const words = [
-  { id: "hello", en: "Hello", ku: "slaw" },
-  { id: "bye", en: "Bye", ku: "xwa hafiz" },
-];
-// var db;
-const request = window.indexedDB.open("dictionary", 1);
+  const words = [
+    { id: "hello", en: "Hello", ku: "slaw" },
+    { id: "bye", en: "Bye", ku: "xwa hafiz" },
+  ];
+  // var db;
+  const request = window.indexedDB.open("dictionary", 1);
 
-request.onerror = (event) => {
-  console.log("error: ");
-};
+  console.log('----------> 1');
 
-request.onsuccess = (event) => {
-  db = request.result;
-  console.log("success: "+ db);
-};
+  request.onerror = (event) => {
+    console.log('----------> 2');
+    console.log("error: ");
+  };
 
-request.onupgradeneeded = (event) => {
-  const db = event.target.result;
-  db.deleteObjectStore('words');
+  request.onsuccess = (event) => {
+    console.log('----------> 3');
+    db = request.result;
+    console.log("success: "+ db);
+    initLoadWords(request);
+  };
 
-  const wordsStore = db.createObjectStore("words", {keyPath: "id"});
-  for (let i in words) {
-    wordsStore.add(words[i]);
+  request.onupgradeneeded = (event) => {
+    console.log('----------> 4');
+    const db = event.target.result;
+    if (db.objectStoreNames.length > 0) {
+      db.deleteObjectStore('words');
+    }
+
+    const wordsStore = db.createObjectStore("words", {keyPath: "id"});
+    for (let i in words) {
+      wordsStore.add(words[i]);
+    }
   }
 }
+
 
 
 //dict
-customElements.define('di-ct',
-  class extends HTMLElement {
-    constructor() {
-      super();
+initLoadWords = (request) => {
+  customElements.define('di-ct',
+    class extends HTMLElement {
+      constructor() {
+        super();
 
-      const pElem = document.createElement('span');
-      const id = this.innerHTML
+        const pElem = document.createElement('span');
+        const id = this.innerHTML
 
-      const shadowRoot = this.attachShadow({mode: 'open'});
-
-
-      //start
-      let request = window.indexedDB.open("dictionary", 1),
-        db,
-        tx,
-        store,
-        index;
+        const shadowRoot = this.attachShadow({mode: 'open'});
 
 
-      request.onerror = function(e) {
-        console.warn("there was an error: ", e);
-      }
+        //start
+        // let request = window.indexedDB.open("dictionary", 1),
+        //   db,
+        //   tx,
+        //   store,
+        //   index;
 
-      request.onsuccess = function(e) {
         db = request.result;
-        tx = db.transaction("words", "readonly");
-        store = tx.objectStore("words");
+        const tx = db.transaction("words", "readonly");
+        const store = tx.objectStore("words");
+        let q1 = store.get('hello');
+        
 
-        db.oerror = function(e) {
-          console.warn("ERROR: ", e.target.errCode);
-        }
-
-        let q1 = store.get(id);
-        q1.onsuccess = function() {
-          console.log('.........', q1.result);
-          if (q1.result === undefined) {
-            pElem.textContent = `**${id}**`;
-          } else {
-            if ('ku' in q1.result) {
-              pElem.textContent = q1.result.ku;
+        console.log('+++++++++++ 1', q1);
+          q1.onsuccess = function() {
+            console.log('.........', q1.result);
+            if (q1.result === undefined) {
+              pElem.textContent = `**${id}**`;
             } else {
-              pElem.textContent = `*${id}*`;
+              if ('ku' in q1.result) {
+                pElem.textContent = q1.result.ku;
+              } else {
+                pElem.textContent = `*${id}*`;
+              }
             }
+            // if ('ku' in q1.result) {
+            //   console.log(q1.result.ku);
+            // }
+            // pElem.textContent = q1.result.ku;
+            shadowRoot.appendChild(pElem);
           }
-          // if ('ku' in q1.result) {
-          //   console.log(q1.result.ku);
-          // }
-          // pElem.textContent = q1.result.ku;
-          shadowRoot.appendChild(pElem);
+
+
+        /*
+        request.onerror = function(e) {
+          console.log('+++++++++++ 2');
+          console.warn("there was an error: ", e);
         }
 
-        tx.oncomplete = function() {
-          db.close();
+        request.onsuccess = function(e) {
+          console.log('+++++++++++ 3');
+          db = request.result;
+          tx = db.transaction("words", "readonly");
+          store = tx.objectStore("words");
+
+          db.oerror = function(e) {
+            console.warn("ERROR: ",e, e.target.errCode);
+          }
+
+          let q1 = store.get(id);
+          q1.onsuccess = function() {
+            console.log('.........', q1.result);
+            if (q1.result === undefined) {
+              pElem.textContent = `**${id}**`;
+            } else {
+              if ('ku' in q1.result) {
+                pElem.textContent = q1.result.ku;
+              } else {
+                pElem.textContent = `*${id}*`;
+              }
+            }
+            // if ('ku' in q1.result) {
+            //   console.log(q1.result.ku);
+            // }
+            // pElem.textContent = q1.result.ku;
+            shadowRoot.appendChild(pElem);
+          }
+
+          tx.oncomplete = function() {
+            db.close();
+          }
         }
+        */
+        //end
       }
-      //end
     }
-  }
-);
+  );
+}
 
+initWords(initLoadWords);
+
+/*
+*/
